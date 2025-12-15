00b84604fb7a841421b06524b2215adf5af689e4
Amol Shinde <shindeamol409@gmail.com>
2025-12-15 11:41:31 +0530

    Refactored backend to load environment variables from api/.env

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+
 dotenv.config();
 
 mongoose
